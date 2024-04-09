5f08238342f3930febdcf591fde505d29f4bcec0
Bhavik-punmiya <118907757+Bhavik-punmiya@users.noreply.github.com>
2024-04-09 19:17:08 +0530

    commit

--- app.py
+++ app.py
@@ -1,47 +1,35 @@
-from flask import Flask,render_template,request
+import streamlit as st
 import pickle
 import numpy as np
 
-popular_df = pickle.load(open('popular.pkl','rb'))
-pt = pickle.load(open('pt.pkl','rb'))
-books = pickle.load(open('books.pkl','rb'))
-similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
+# Load data
+popular_df = pickle.load(open('popular.pkl', 'rb'))
+pt = pickle.load(open('pt.pkl', 'rb'))
+books = pickle.load(open('books.pkl', 'rb'))
+similarity_scores = pickle.load(open('similarity_scores.pkl', 'rb'))
 
-app = Flask(__name__)
+# Display popular books
+st.title("Popular Books")
+st.write("This is a list of popular books.")
 
-@app.route('/')
-def index():
-    return render_template('index.html',
-                           book_name = list(popular_df['Book-Title'].values),
-                           author=list(popular_df['Book-Author'].values),
-                           image=list(popular_df['Image-URL-M'].values),
-                           votes=list(popular_df['num_ratings'].values),
-                           rating=list(popular_df['avg_rating'].values)
-                           )
+for index, row in popular_df.iterrows():
+    st.image(row['Image-URL-M'], use_column_width=True)
+    st.write(f"**{row['Book-Title']}** by {row['Book-Author']}")
+    st.write(f"Ratings: {row['num_ratings']}, Average Rating: {row['avg_rating']}")
 
-@app.route('/recommend')
-def recommend_ui():
-    return render_template('recommend.html')
+# Recommendation UI
+st.title("Book Recommendation")
+user_input = st.text_input("Enter a book title to get recommendations:")
 
-@app.route('/recommend_books',methods=['post'])
-def recommend():
-    user_input = request.form.get('user_input')
-    index = np.where(pt.index == user_input)[0][0]
-    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:9]
+if user_input:
+    try:
+        index = np.where(pt.index == user_input)[0][0]
+        similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:9]
 
-    data = []
-    for i in similar_items:
-        item = []
-        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
-
-        data.append(item)
-
-    print(data)
-
-    return render_template('recommend.html',data=data)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+        st.write("Recommended Books:")
+        for i in similar_items:
+            temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+            st.image(temp_df['Image-URL-M'].values[0], use_column_width=True)
+            st.write(f"**{temp_df['Book-Title'].values[0]}** by {temp_df['Book-Author'].values[0]}")
+    except IndexError:
+        st.write("No recommendations found for the entered book title.")
